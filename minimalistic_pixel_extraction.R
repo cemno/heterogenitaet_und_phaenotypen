ce510e14b886c2d72be260b85a9311b96a103351
cemno <clemensstephany@gmail.com>
2021-06-02 16:57:48 +0200

    Added markdown and put a lot of work in it. (a lot lot, but tabs are cool)

--- minimalistic_pixel_extraction.R
+++ minimalistic_pixel_extraction.R
@@ -1,8 +1,8 @@
 # Loading required libraries
-library(raster)
-library(tidyverse)
-library(parallel)
-library(sf)
+library(raster, quietly = TRUE)
+library(tidyverse, quietly = TRUE)
+library(parallel, quietly = TRUE)
+library(sf, quietly = TRUE)
 
 ## Starting Cluster
 threads <- 3
@@ -23,18 +23,19 @@ raster_raw <- raster::extract(SIF_600_1115_A, plots, cellnumber = TRUE, df = TRU
 raster <- filter(raster_raw, weight == 1)
 
 ## Join raster data and plot data
-# Maybe use left join (or full join?) to get empty rows for plot ids that didn't got any cells
 raster <- left_join(raster, plots, by = "ID")
 
 ## Count of captured cells per plot
 rename(data.frame(table(raster$PLOTID)), PlotID = Var1)
 print(paste0("Für folgende ID wurde kein Pixel extrahiert: ", setdiff(unique(raster$ID), seq(1:nrow(plots)))))
 # Question: How are smaller pixels (caused by rectification) are handled? Do they count on there own? Or are they part of another pixel?
-# Or does the Picture has unequal pixels in it? Can we extract pixel size?
+# Or does the Picture has unequal pixels in it?
 
 ## Aggregate Cell Data
 bands_mean <- summarise(group_by(raster, PLOTID), across(starts_with("Georef"), mean))
 raster_mean <- left_join(bands_mean, plots, by = "PLOTID")
+# WARNING: Data is moved by 1000 to remove float values. This explains negative values.
 
 ## Closing cluster
 endCluster()
+
